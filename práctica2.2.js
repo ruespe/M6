c7eb2ad490ac4c59a5bc91c1aa1c9dd5c0927d6c
ruespe <rubenespin84@gmail.com>
2025-10-08 18:20:02 +0200

    Practica2.2.js 1 hecho

---
+++ práctica2.2.js
@@ -0,0 +1,27 @@
+// 1. A partir del següent Array
+const headItem = ['Codi','Imatge', 'Descripció', 'Quantitat', 'Preu',
+'Import'];
+
+let table = document.createElement("table")
+let thead = document.createElement("thead")
+let tr = document.createElement("tr")
+
+headItem.forEach(item => {
+   tr.insertAdjacentHTML("beforeend",`<th>${item}</th>`)
+});
+
+thead.appendChild(tr)
+table.appendChild(thead)
+document.querySelector("main").appendChild(table)
+
+const products = [
+[101,'steelseires-arctis-5-rgb-negros.webp', 'Steelseires Arctis 5 Auriculars Gaming RGB Negres', 108.59],
+ [102,'1202-agfa-photo-ac7000-camara-deportiva-16mp.webp', 'Agfa
+Photo AC7000 Càmera Esportiva 16MP', 119.50],
+ [103,'1920-xiaomi-poco-m3-pro-5g-4-64gb-amarillo-libre.webp',
+'Xiaomi POCO M3 Pro 5G 4/64GB Groc LLiure', 315.99],
+ [104,'logitech.webp', 'Logitech G Saitek X52 Flight Control System
+Sistema de Control de Vol', 158.60],
+ [105,'115-msi-raider.webp', 'MSI Raider GE77HX 12UGS-020ES Intel
+Core i9-12900HX/64GB/2TB SSD/RTX 3070Ti/17.3"', 3599.00]
+];
