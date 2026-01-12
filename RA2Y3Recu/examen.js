c7420336b1f6c12592db777d0f10f47d537c0ec2
ruespe <rubenespin84@gmail.com>
2026-01-12 19:19:24 +0100

    casi hecho

--- RA2Y3Recu/examen.js
+++ RA2Y3Recu/examen.js
@@ -42,7 +42,10 @@ document.addEventListener("DOMContentLoaded", () => {
     nuevaImageList.sort((a, b) => b.codi - a.codi);
 
   const div = document.createElement("div");
+  div.classList.add("lista")
   document.body.appendChild(div);
+
+
 
   // Usar el template strings para mostrar cada objeto
   nuevaImageList.forEach(item => {
@@ -53,6 +56,55 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   const botonSaveData = document.createElement("button")
+  document.body.appendChild(botonSaveData)
+  botonSaveData.innerHTML = "Save";
+
+  // Botón Save funcionamiento
+  botonSaveData.addEventListener("click", () => {
+    // 1.Productos escogidos random
+    // 3.Evitar repetición productos
+    let copiaNuevaImageList = [...nuevaImageList];
+    let aleatorios = copiaNuevaImageList.sort(() => Math.random() - 0.5).slice(0, 6)
+    console.log(aleatorios)
+
+    // 2.Guardar fecha actual
+    //5. La data ha d'incloure dia, mes, any i l'hora (hora,minuts,segons)
+    let fechaActual = new Date().toLocaleString();
+    console.log(fechaActual)
 
 
+    // 4.Guardar en una sola clave y en JSON
+      // Guardar en LocalStorage
+
+      // Recuperar de LocalStorage
+    let datosGuardados = JSON.parse(localStorage.getItem("datos"))
+    console.log("Datos recuperados", datosGuardados)
+
+    //6. Quan es prem el botó Save Data s'elimina la llista creada en el punt 1
+    document.querySelector(".lista").remove();
+  })
+
+  let botonMostrar = document.createElement("button");
+  document.body.appendChild(botonMostrar)
+  botonMostrar.classList.add("mostrar")
+  botonMostrar.innerHTML = "Mostrar";
+
+// 3. Mitjançant un altre botó Mostrar creat amb createElement() es recuperen les dades: (3 punts)
+  //  1. Aquest botó només es mostra si hi han dades desades a localStorage
+if(localStorage.length > 0){
+  botonMostrar.style.display = "block"
+}else{
+  botonMostrar.style.display = "none"
+}
+  // 2. El títol en un <h2> format pel nom de la imatge sense l'extensió i en majúscula.
+
+  // 3. Les imatges:
+    // 1. El títol i la imatge estan continguts en un <div>
+    // 2. La mida de les imatges serà de 100px establert com a propietat de l'element <img>
+
+  // 4. Data i hora que s’han desat a localStorage.
+
+  botonMostrar.addEventListener("click", () => {
+
+  })
 });
